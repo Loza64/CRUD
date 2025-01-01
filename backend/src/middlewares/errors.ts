1b0e5dfbbeb3bfdf3ca14bb5fff1536fd23a53d7
Loza64 <robertoloxa88@gmail.com>
2024-12-31 19:28:23 -0600

    changes

--- backend/src/middlewares/errors.ts
+++ backend/src/middlewares/errors.ts
@@ -1,7 +1,7 @@
-import {Response, NextFunction } from 'express'
+import { Response, NextFunction } from 'express'
 import { error } from '../../config'
 
 export const errorHandler = (err: any, res: Response, next: NextFunction) => {
     error(err.message)
-    res.status(err.status || 500).json({ message: err.message || 'Something went wrong' })
+    res.status(err.status || 500).json({ status: "error", message: err.message || 'Something went wrong' })
 }
