bd12374e35b2de60a549cc8bad95d8c9ccf8547d
Loza64 <robertoloxa88@gmail.com>
2025-01-03 19:10:30 -0600

    changes

--- backend/src/validators/image.ts
+++ backend/src/validators/image.ts
@@ -1,5 +1,6 @@
 const { body } = require("express-validator");
 import { UploadedFile } from 'express-fileupload';
+import { remove } from 'fs-extra'
 
 export const imageValidation = [
     body("image").custom((value, { req }) => {
@@ -8,6 +9,7 @@ export const imageValidation = [
             throw new Error("No se ha enviado ninguna imagen");
         }
         if (!photo.mimetype.includes("image")) {
+            remove(photo.tempFilePath);
             throw new Error("El archivo no es una imagen");
         }
         return true;
