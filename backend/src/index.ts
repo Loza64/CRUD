c7310d1ea577bd294d72249b3eeeb7ce6c107529
Loza64 <robertoloxa88@gmail.com>
2025-01-04 22:29:43 -0600

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
-import { port, runing } from './src/config';
-import app from './src/app';
+import { port, runing } from './config';
+import app from './app';
 
 const server = createServer(app);
 
