a222934ed191f678dac48dce378eb7b00f398da0
Roberto Loza <robertoloxa88@gmail.com>
2025-04-10 17:34:53 -0600

    implement validate image content

--- backend/src/middlewares/validator.ts
+++ backend/src/middlewares/validator.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Response, Request } from "express";
 import { validationResult } from "express-validator";
 import { error } from "../config";
+import { UploadedFile } from "express-fileupload";
+import { remove } from "fs-extra";
 
 interface ValidationError {
     type: string;
@@ -13,6 +15,16 @@ export const validate = (req: Request, res: Response, next: NextFunction) => {
     const validator = validationResult(req);
 
     if (!validator.isEmpty()) {
+
+        const image = req.files?.image as UploadedFile | UploadedFile[];
+
+        if (image) {
+            if (Array.isArray(image)) {
+                image.forEach((img) => remove(img.tempFilePath));
+            } else {
+                remove(image.tempFilePath);
+            }
+        }
 
         const errors = validator.array() as ValidationError[];
 
--- backend/src/validators/image.ts
+++ backend/src/validators/image.ts
@@ -1,17 +1,45 @@
+import { load } from 'nsfwjs';
+import { remove } from 'fs-extra';
 import { body } from "express-validator";
+import { createCanvas, loadImage } from 'canvas';
 import { UploadedFile } from 'express-fileupload';
-import { remove } from 'fs-extra'
 
 export const imageValidation = [
-    body("image").custom((value, { req }) => {
+    body("image").custom(async (value, { req }) => {
         const photo = req.files?.image as UploadedFile;
+
         if (!photo) {
             throw new Error("No se ha enviado ninguna imagen");
         }
-        if (!photo.mimetype.includes("image")) {
-            remove(photo.tempFilePath);
+
+        if (Array.isArray(photo)) {
+            throw new Error("Se ha enviado más de una imagen");
+        }
+
+        if (!photo.mimetype?.includes("image")) {
+            await remove(photo.tempFilePath);
             throw new Error("El archivo no es una imagen");
         }
+
+
+        let nsfw = await load();
+
+        const image = await loadImage(photo.tempFilePath);
+        const canvas = createCanvas(image.width, image.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(image, 0, 0);
+
+        const predictions = await nsfw.classify(canvas as unknown as HTMLCanvasElement);
+        console.log("Predicciones NSFW:", predictions);
+
+        const nsfwCategories = { Porn: 'Porn', Hentai: 'Hentai', Data: 'Sexy' }
+        const isNSFW = predictions.some(({ className, probability }) => nsfwCategories[className] && probability > 0.75);
+
+        if (isNSFW) {
+            await remove(photo.tempFilePath);
+            throw new Error("La imagen contiene contenido inapropiado");
+        }
+
         return true;
     }),
 ];
